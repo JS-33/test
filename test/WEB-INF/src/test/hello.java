9038bd72c08a0551203827052da493437b26d916
Zach76 <thompson_z-13b@stu.kbc.ac.jp>
2015-07-14 14:38:07 +0900

    トンプソンザカライアジュン

--- test/WEB-INF/src/test/hello.java
+++ test/WEB-INF/src/test/hello.java
@@ -14,7 +14,7 @@ out.println("<head>");
 out.println("<title>Hello World!</title>");
 out.println("</head>");
 out.println("<body>");
-out.println("<h1>にっこにっこにー</h1>");
+out.println("<h1>山頂アタックです！</h1>");
 out.println("</body>");
 out.println("</html>");
 }
