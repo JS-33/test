4d5c8f9e8abab8aaa2e4652fb430776377efc527
Zach76 <thompson_z-13b@stu.kbc.ac.jp>
2015-07-15 09:38:25 +0900

    ゴギガ・ガガギゴ

--- hikki-/WEB-INF/src/kyouyuu.java
+++ hikki-/WEB-INF/src/kyouyuu.java
@@ -5,7 +5,6 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 public class kyouyuu extends HttpServlet {
 
 public void doGet(HttpServletRequest request, HttpServletResponse response)
